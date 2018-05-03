7144e521b223b3a7919042a33357dcf01053cbfe
Pepo <pepo.ospina@gmail.com>
2018-05-03 12:52:32 +0200

    merge f826a4b4

--- src/main/java/org/collectiveone/modules/activity/repositories/NotificationRepositoryIf.java
+++ src/main/java/org/collectiveone/modules/activity/repositories/NotificationRepositoryIf.java
@@ -14,6 +14,9 @@ public interface NotificationRepositoryIf extends CrudRepository<Notification, U
 
 	Notification findById(UUID notificationId);
 	
+	@Query("SELECT notif FROM Notification notif WHERE notif.subscriber.user.c1Id = ?1 AND notif.id IN ?2")
+	List<Notification> findByIdIn(UUID userId, List<UUID> notificationIds);
+	
 	@Query("SELECT notif FROM Notification notif JOIN notif.activity act WHERE notif.subscriber.user.c1Id = ?1 ORDER BY act.timestamp DESC")
 	List<Notification> findOfUser(UUID userId, Pageable page);
 	
@@ -25,6 +28,15 @@ public interface NotificationRepositoryIf extends CrudRepository<Notification, U
 			+ "OR act.modelCardWrapper.id IN ?4) "
 			+ "ORDER BY act.timestamp DESC")
 	List<Notification> findOfUserInSections(UUID userId, NotificationState state, List<UUID> sectionIds, List<UUID> cardWrappersIds, Pageable page);	
+	
+	
+	@Query("SELECT notif FROM Notification notif "
+            + "JOIN notif.activity act "
+            + "WHERE notif.subscriber.user.c1Id = ?1 "
+            + "AND notif.inAppState = ?2 "
+            + "AND act.initiative.id IN ?3 "
+            + "ORDER BY act.timestamp DESC")
+    List<Notification> findOfUserInInitiatives(UUID userId, NotificationState state, List<UUID> initiativeIds, Pageable page);
 	
 	List<Notification> findBySubscriber_User_C1IdAndInAppState(UUID userId, NotificationState notificationState);
 	
